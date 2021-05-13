cb7cae593cb2f54f2debfd54620a775c3a7c24db
K0N5T1 <konsti.wohner@outlook.de>
2021-05-14 00:10:06 +0200

    Added CSV Export functionality to processing.html All temporary files are removed after request

--- export.py
+++ export.py
@@ -1,3 +1,5 @@
+import zipfile
+
 from flask import Blueprint, jsonify, request, send_file, url_for, after_this_request
 import pandas as pd
 from db_actions import PostgersqlDBManagement
@@ -6,6 +8,9 @@ from random import choice
 from string import ascii_uppercase
 import os
 from io import BytesIO
+from pathlib import Path
+from os.path import basename
+import shutil
 
 export_interface = Blueprint(
     'export_interface', __name__, url_prefix="/export_interface")
@@ -47,7 +52,32 @@ def export_folder_pictures():
 
 @export_interface.route("/download/csv")
 def download_csv():
-    return ""
+    database = get_db_instance()
+    id = ''.join(choice(ascii_uppercase) for i in range(12))
+    folder_name = Path(os.path.join(".\\upload", id))
+    folder_name.mkdir(parents=True, exist_ok=True)
+
+    #creating a zíp file for every table
+    for table_name in database.get_view_names():
+        data_list = [dict(row) for row in database.get_table(table_name, filter=request.values.to_dict())]
+        if len(data_list) > 0:
+            df = pd.DataFrame(data_list)
+            df.columns = database.get_table_columns(table_name)
+            df.to_csv(os.path.join(folder_name, f"{table_name}.csv"))
+    #zipping files in memory
+    memory_file = BytesIO()
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        for folderName, subfolders, filenames in os.walk(folder_name):
+            for filename in filenames:
+                filePath = os.path.join(folderName, filename)
+                zf.write(filePath, basename(filePath))
+    memory_file.seek(0)
+    #removing temporary files from upload folder
+    @after_this_request
+    def clear_dir(response):
+        shutil.rmtree(folder_name, ignore_errors=True)
+        return response
+    return send_file(memory_file, attachment_filename="testing.zip", as_attachment=True)
 
 
 @export_interface.route("/download/excel")
